d307320ac6a1fc39de506f23457cf97840848b11
rememberTHAT <44797581+rememberTHAT@users.noreply.github.com>
2022-02-28 16:55:32 +0800

    Update exercise2.cpp

--- chapter2/exercise2.cpp
+++ chapter2/exercise2.cpp
@@ -2,10 +2,13 @@
 
 int main(){
     using namespace std;
+    
     double num, ans;
     cout<<"Please input a number: (unit: long) "<<endl;
     cin>>num;
     ans=num*220;
+    
     cout<<"Answer is : "<<ans<<endl;
+    
     return 0;
 }
