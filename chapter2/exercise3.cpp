5fec93026737ae94b26098dfefed0a24066f3b47
rememberTHAT <44797581+rememberTHAT@users.noreply.github.com>
2022-02-28 17:18:25 +0800

    Create exercise3.cpp

---
+++ chapter2/exercise3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void line1(){
+     cout<<"Three blind mice"<<endl;
+}
+
+void line2(){
+     cout<<"See how they run" <<endl;
+}
+
+int main(){
+    
+    line1();
+    line1();
+  
+    line2();
+    line2();
+  
+    return 0;
+}
