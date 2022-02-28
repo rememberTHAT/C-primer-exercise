cd34f88ef24f906f64889c20b2c105c40e67dc68
rememberTHAT <44797581+rememberTHAT@users.noreply.github.com>
2022-02-28 17:24:41 +0800

    Create exercise4.cpp

---
+++ chapter2/exercise4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age, age_m;
+    
+    cout<<"Enter your age: "<<endl;
+    cin>>age;
+    age_m = 12*age;
+    
+    cout<<"Your age contains "<<age_m<<"momth"<<endl;
+    
+    return 0;
+}
