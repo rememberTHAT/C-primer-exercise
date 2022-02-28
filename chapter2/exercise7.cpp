52b461d520b19d3b923de140b9444a3f10b5b8c8
rememberTHAT <44797581+rememberTHAT@users.noreply.github.com>
2022-02-28 17:38:39 +0800

    Create exercise7.cpp

---
+++ chapter2/exercise7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int hour, mins;
+    
+    cout<<"Enter the number of hours : "<<endl;
+    cin>>hour;
+	cout<<"Enter the number of minutes : "<<endl;
+	cin>>mins;
+    cout<<"Time:  "<<hour<<" : "<<mins<<endl;
+  
+    return 0; 
+}
