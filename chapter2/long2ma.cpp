fe1c1bcbffa4555945dd36bb4da0ff24cefbfbfa
rememberTHAT <44797581+rememberTHAT@users.noreply.github.com>
2022-02-28 16:53:56 +0800

    Update long2ma.cpp

--- chapter2/long2ma.cpp
+++ chapter2/long2ma.cpp
@@ -1,1 +1,11 @@
+#include<iostream>
 
+int main(){
+    using namespace std;
+    double num, ans;
+    cout<<"Please input a number: (unit: long) "<<endl;
+    cin>>num;
+    ans=num*220;
+    cout<<"Answer is : "<<ans<<endl;
+    return 0;
+}
