b8200f2f5cc765eaa671c75027e61515b57155ed
rememberTHAT <44797581+rememberTHAT@users.noreply.github.com>
2022-03-07 08:25:33 +0800

    Update exercise3_1.cpp

--- chapter3/exercise3_1.cpp
+++ chapter3/exercise3_1.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    
+const int trans = 12;
+
+int main()
+{   int height=0;
 
 
+    cout<<"Please enter your height:__\b\b"<<endl;
+    cin>>height;
+
+    cout<<"Your height is "<<height/trans<<" Inches and "<<height%trans<<" foot."<<endl;
+
+    return 0;
 }
